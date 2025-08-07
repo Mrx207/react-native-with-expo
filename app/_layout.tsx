ec4dfed9a5de04bb768deacb6dc8b16054772904
mrx207 <rushi.deshmukh2071999@gmail.com>
2025-08-07 17:57:39 +0530

    todo app created

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,62 +1,16 @@
-import { Colors } from "@/constants/Colors";
-import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
-import { Appearance } from "react-native";
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+import React from "react";
+import { StyleSheet } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-export default function RootLayout() {
-  const colorScheme = Appearance.getColorScheme() || "light";
-  const theme = colorScheme === "dark" ? Colors.dark : Colors.light;
-  const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
+export default function _layout() {
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: { backgroundColor: theme.background },
-        headerTintColor: theme.text,
-        headerShadowVisible: false,
-      }}
-    >
-      <Stack.Screen
-        name="index"
-        options={{
-          headerShown: true,
-          title: "Menu",
-          headerTitle: "Coffee Shop",
-        }}
-      />
-      <Stack.Screen
-        name="menu"
-        options={{
-          headerShown: true,
-          title: "Menu Items",
-          headerTitle: "Menu list",
-        }}
-      />
-      <Stack.Screen
-        name="contact"
-        options={{
-          headerShown: true,
-          title: "Contact",
-          headerTitle: "Contact Us",
-        }}
-      />
-      <Stack.Screen name="+not-found" options={{ headerShown: false }} />
-    </Stack>
+    <SafeAreaProvider>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
+    </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({});
--- app/index.jsx
+++ app/index.jsx
@@ -1,90 +1,126 @@
-import icedCoffeeImage from "@/assets/images/iced-coffee.png";
-import { Link } from "expo-router";
-import React from "react";
+import React, { useRef, useState } from "react";
 import {
-  ImageBackground,
-  Pressable,
+  Button,
+  FlatList,
   StyleSheet,
   Text,
+  TextInput,
   View,
 } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const app = () => {
+export default function index() {
+  const [todos, setTodos] = useState([
+    { name: "Buy coffee" },
+    { name: "Read a book" },
+  ]);
+  const [inputValue, setInputValue] = useState("");
+  const [editIndex, setEditIndex] = useState(null);
+
+  const inputRef = useRef(null);
+
+  const handleSubmit = () => {
+    if (!inputValue.trim()) {
+      alert("Please enter a todo item");
+      inputRef.current.focus();
+      return;
+    }
+
+    if (todos.some((todo) => todo.name === inputValue.trim())) {
+      alert("Todo already exists");
+      inputRef.current.focus();
+      return;
+    }
+    console.log(editIndex);
+    if (editIndex !== null) {
+      const updatedTodos = [...todos];
+      updatedTodos[editIndex] = { name: inputValue.trim() };
+      setTodos(updatedTodos);
+      setEditIndex(null);
+    } else {
+      setTodos([...todos, { name: inputValue.trim() }]);
+    }
+    setInputValue("");
+    inputRef.current.clear();
+    inputRef.current.focus();
+    // console.log(todos);
+  };
+
+  const handleUpdate = (name) => {
+    console.log("Updating todo:", name);
+    const index = todos.findIndex((todo) => todo.name === name);
+    console.log("Index found:", index);
+    if (index !== -1) {
+      setInputValue(name);
+      setEditIndex(index);
+      inputRef.current.focus();
+    }
+  };
   return (
-    <View style={styles.container}>
-      <ImageBackground
-        source={icedCoffeeImage}
-        style={styles.backgroundImage}
-        resizeMode="cover"
-      >
-        <Text style={styles.titleContainer}>Coffee shop</Text>
-        <Link href="/menu" style={{ marginHorizontal: "auto" }} asChild>
-          <Pressable style={styles.button}>
-            <Text style={styles.buttonText}>Menu</Text>
-          </Pressable>
-        </Link>
-        <Link href="/contact" style={{ marginHorizontal: "auto" }} asChild>
-          <Pressable style={styles.button}>
-            <Text style={styles.buttonText}>Contact Us</Text>
-          </Pressable>
-        </Link>
-      </ImageBackground>
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text>Todo App</Text>
+        <TextInput
+          style={styles.inputText}
+          placeholder="Add a new todo"
+          placeholderTextColor="black"
+          value={inputValue}
+          onChangeText={(text) => setInputValue(text)}
+          ref={inputRef}
+        />
+        <Button title="Submit todo" onPress={handleSubmit} />
+        <Text style={{ margin: 10 }}>Todos:</Text>
+        {todos.length === 0 && <Text>No todos available</Text>}
+        <FlatList
+          data={todos}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item }) => (
+            <View style={styles.todoItem}>
+              <Text>{item.name}</Text>
+              <Button
+                title="Edit"
+                style={styles.editButton}
+                onPress={() => handleUpdate(item.name)}
+              />
+              <Button
+                title="Delete"
+                style={styles.deleteButton}
+                onPress={() => {
+                  setTodos(todos.filter((todo) => todo.name !== item.name));
+                }}
+              />
+            </View>
+          )}
+        />
+      </View>
+    </SafeAreaView>
   );
-};
-
-export default app;
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
+  todoItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
-    backgroundColor: "#F5FCFF",
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
   },
-  titleContainer: {
-    width: "100%",
-    fontSize: 42,
-    textAlign: "center",
+  inputText: {
+    borderWidth: 1,
+    borderColor: "black",
+    padding: 10,
     margin: 10,
+    borderRadius: 5,
+  },
+  deleteButton: {
+    marginTop: 10,
+    backgroundColor: "red",
     color: "white",
-    fontWeight: "bold",
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
   },
-  link: {
-    width: "100%",
-    fontSize: 42,
-    textAlign: "center",
-    margin: 10,
-    color: "white",
-    fontWeight: "bold",
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
-    textDecorationLine: "underline",
-  },
-  buttonText: {
-    width: "100%",
-    fontSize: 16,
-    textAlign: "center",
-    color: "white",
-    fontWeight: "bold",
-    padding: 4,
-
-
-  },
-  button:{
-    height: 50,
-    borderRadius: 20,
-    justifyContent: "center",
-    backgroundColor: 'rgba(0, 0, 0, 0.75)',
-    padding:6
-  },
-  backgroundImage: {
-    width: "100%",
-    height: "100%",
-    justifyContent: "center",
-    flex: 1,
-    resizeMode: "cover",
-    alignItems: "center",
-    opacity: 0.8,
+  editButton: {
+    marginTop: 10,
+    backgroundColor: "green",
+    color: "green",
   },
 });
